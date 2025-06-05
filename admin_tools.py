fe3dc4db32b5ca759cdf812e685298da67894055
Josue A. Nonalaya <64487590+JosueNonalaya@users.noreply.github.com>
2025-06-05 11:19:46 -0300

    Add files via upload

---
+++ admin_tools.py
@@ -0,0 +1,72 @@
+"""
+AÇOES:
+cadastrar filmes
+remover filmes
+ver quantidade de filmes laugados
+ver total do dinheiro ganho no dia
+"""
+# Catalogo de filmes, para cada genero existe uma lista com os nomes dos filmes
+catalg_films = {"aventura":["Back to the Future I","Goonies","Senhor dos aneis"],
+                   "açao":["Terminator II", "James Bond:NO TIME TO DIE","Missão Impossível III"],
+                   "comédia":["Escola de Rock", "Free Guy"]
+                   }
+films_alugados = 0      #Quantidades de filmes no estado: "alugados"
+val_alugados = 0        #Somatoria do valor de cada filme "alugado" no dia
+generos = [
+        "1. Aventura",
+        "2. Ação",
+        "3. Quantidade de filmes alugados no dia",
+        "4. Valor total do dia",
+        "5. Sair"
+    ]
+
+
+# Imprimindo menu com as opções e recebendo seleção da ferramenta
+def menu(options):
+    print("***** BEM-VINDO ADMINISTRADOR *****")
+    for option in options:
+        print(option)
+    return get_int_option(1, 5, "Digite o numero da opçao selecionada(1 a 5): " )
+
+
+# Verifica que a escolha da opçao inserida esteja dentro das fornecidas no menu
+def get_int_option(min, max, message):
+    while True:
+        try:
+            op = int(input(message))
+            if op >= min and op <= max:
+                return op
+            else:
+                print("O valor deve estar entre", min, "e", max)
+        except:
+            print("Valor inválido")
+
+# Adicionar filme
+def add_film()
+    print("-- Adicionar Filme ---")
+    genero = input("Em qual genero  voce gostaria de adicionar o filme")
+
+# Função principal
+def main(option):
+    while True:
+        op = menu(option)
+        if op == 1:
+            pass
+        elif op == 2:
+            pass
+        elif op == 3:
+            pass
+        elif op == 4:
+            pass
+        else:
+            break
+
+if __name__ == '__main__':
+    menu_adm = [
+        "1. Cadastrar Filme",
+        "2. Remover Filme",
+        "3. Quantidade de filmes alugados no dia",
+        "4. Valor total do dia",
+        "5. Sair"
+    ]
+main(menu_adm)
